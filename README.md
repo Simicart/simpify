2dac631c9da3ffd6d5b96d2182261469797dfab0
lanapone <lanapone@lanapones-MBP.lan>
2021-08-25 09:54:08 +0700

    add lint command"

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # simpify
 
+Copy __.env.sample__ to __.env__, config your env
+
 ```
 yarn install
 yarn start
--- src/components/App/core/Main/main.js
+++ src/components/App/core/Main/main.js
@@ -11,7 +11,7 @@ let hiddenSplash = false;
 
 const App = (props) => {
     const configData = useConfig();
-    
+
     if (configData) {
         if (!hiddenSplash) {
             try {
